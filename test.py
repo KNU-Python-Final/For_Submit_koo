2d86f3bb23c7ab990a067b236990906e88cc3c7a
김민재 <76814013+tangerinem@users.noreply.github.com>
2023-11-27 16:07:15 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pacman_2
 import round3
 import round2_1
-# pacman_2.pacman()
-# round2_1.round2()
-# round3.round3()
+# pacman_2.pacman() #1라운드 
+# round2_1.round2() #2라운드
+# round3.round3()   #3라운드
