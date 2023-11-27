f6eb7c2b172090ddd3f921d2f5d696d2d2c2bd2e
UsernameisKoo <syonkoo@naver.com>
2023-11-27 16:08:42 +0900

    변경

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import pacman_2
 import round3
 import round2_1
-# pacman_2.pacman()
-# round2_1.round2()
-# round3.round3()
+#pacman_2.pacman()
+#round2_1.round2(1) #() 안에 소리 듣고 싶으면 1, 안 듣고싶으면 0 넣기
+#round3.round3(1) #() 안에 소리 듣고 싶으면 1, 안 듣고싶으면 0 넣기
+
