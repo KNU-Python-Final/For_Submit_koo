3aef51bcd1db9b20ed3f11f7afd5740fc2931142
UsernameisKoo <149701913+UsernameisKoo@users.noreply.github.com>
2023-11-27 16:12:07 +0900

    주석 추가

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pacman_2
 import round3
 import round2_1
-# pacman_2.pacman() #1라운드 
-# round2_1.round2() #2라운드
-# round3.round3()   #3라운드
+#pacman_2.pacman()  #1라운드
+#round2_1.round2(1) #2라운드 - () 안에 소리 듣고 싶으면 1, 안 듣고싶으면 0 넣기
+#round3.round3(1) #3라운드 - () 안에 소리 듣고 싶으면 1, 안 듣고싶으면 0 넣기
